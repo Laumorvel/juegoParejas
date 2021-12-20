318d522d9648d6aceab5175a96cfc49fe6c73e27
lauramolez <lauramorelez@gmail.com>
2021-12-20 14:25:34 +0100

    empezado proyecto

--- juegoParejas/src/app/app.module.ts
+++ juegoParejas/src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { GameCardComponent } from './carta/game-card/game-card.component';
+import { RestartDialogComponent } from './carta/restart-dialog/restart-dialog.component';
+import { CartaModule } from './carta/carta.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameCardComponent,
+    RestartDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    CartaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
